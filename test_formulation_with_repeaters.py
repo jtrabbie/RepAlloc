45b01ec96de910de92bb6373d4bbca5798a31b0e
julian.rabbie-tudelft <j.t.rabbie@student.tudelft.nl>
2019-08-28 17:39:12 +0200

    Working formulation solver with single repeater

--- test_formulation_with_repeaters.py
+++ test_formulation_with_repeaters.py
@@ -11,36 +11,36 @@ num_pairs = len(list(unique_pairs))  # number of unique customer pairs
 num_nodes = 16  # number of non-customer nodes
 R = 1  # maximum number of repeaters
 M = num_pairs  # dummy parameter for linking constraint
+# Set fixed seed for reproducibility
+np.random.seed(1234)
 
 
 def create_graph(draw=False):
-    # Set fixed seed for reproducibility
-    np.random.seed(1234)
     # Create a graph and add random weights
     graph = nx.fast_gnp_random_graph(n=num_cities + num_nodes, p=0.4, seed=np.random)
     # Change nodes to four Dutch cities
-    mapping = {3: city_list[0], 7: city_list[1], 4: city_list[2], 19: city_list[3]}
+    mapping = {3: city_list[0], 4: city_list[2], 5: 3, 6: 4, 7: city_list[1], 8: 5, 9: 6, 10: 7,
+               11: 8, 12: 9, 13: 10, 14: 11, 15: 12, 16: 13, 17: 14, 18: 15, 19: city_list[3], 20: 16}
     graph = nx.relabel_nodes(graph, mapping)
     for (u, v) in graph.edges():
-        if u in city_list and v in city_list:
-            # Set weight from city to city directly to inf
-            graph.edges[u, v]['weight'] = np.inf
-        else:
-            graph.edges[u, v]['weight'] = np.random.randint(1, 10)
+        graph.edges[u, v]['weight'] = np.random.randint(1, 10)
+    color_map = ['blue'] * len(graph.nodes)
+    # Save seed for drawing
+    global numpy_seed
+    numpy_seed = np.random.get_state()
+    for idx, node in enumerate(graph.nodes):
+        if type(node) == str:
+            color_map[idx] = 'olive'
     if draw:
-        color_map = ['blue'] * len(graph.nodes)
-        for idx, node in enumerate(graph.nodes):
-            if type(node) == str:
-                color_map[idx] = 'olive'
         plt.subplots()
         nx.draw(graph, with_labels=True, font_weight='bold',
                 node_color=color_map)
         plt.show()
 
-    return graph
+    return graph, color_map, mapping
 
 
-def solve_cplex(graph):
+def solve_cplex(graph, color_map, mapping):
     # Create new CPLEX model
     prob = cplex.Cplex()
     # Set objective to minimization
@@ -50,22 +50,37 @@ def solve_cplex(graph):
     add_variables(prob=prob, graph=graph)
     # Solve for the optimal solution and print
     prob.write('test_after_colgen.lp')
+    print('Total number of variables:', prob.variables.get_num())
     prob.solve()
     for i, x in enumerate(prob.solution.get_values()):
         if x > 0.0:
-            path = prob.variables.get_names(i)
-            print('Chosen path ', x, 'x:', path, 'with cost', prob.objective.get_linear(path))
+            var_name = prob.variables.get_names(i)
+            if 'y' in var_name:
+                rep_node_chosen = int(var_name[2::])
+            else:
+                print('Chosen path', x, 'x:', var_name, 'with cost', prob.objective.get_linear(var_name))
 
     print('Optimal objective value:', prob.solution.get_objective_value())
 
+    # Draw graph with repeater placement
+    plt.subplot(211)
+    np.random.set_state(numpy_seed)
+    nx.draw(graph, with_labels=True, font_weight='bold',
+            node_color=color_map)
+    color_map[list(mapping.keys())[list(mapping.values()).index(rep_node_chosen)]] = 'pink'
+    plt.subplot(212)
+    np.random.set_state(numpy_seed)
+    nx.draw(graph, with_labels=True, font_weight='bold',
+            node_color=color_map, seed=123)
+    plt.show()
 
 def add_constraints(prob):
     # Constraints for linking path variables to repeater variables
-    link_con_names = ['LinkCon_' + str(s) for s in range(1, num_nodes + 1)]
+    link_con_names = ['LinkCon_' + str(s) for s in range(num_nodes)]
     prob.linear_constraints.add(rhs=[0] * num_nodes,
                                 senses=['L'] * num_nodes, names=link_con_names)
     # Add repeater variables with a column in the linking constraint
-    var_names = ['y_' + str(s) for s in range(1, num_nodes + 1)]
+    var_names = ['y_' + str(s) for s in range(num_nodes)]
     link_constr_column = []
     for i in range(num_nodes):
         link_constr_column.append([[i], [-M]])
@@ -82,17 +97,24 @@ def add_constraints(prob):
 
 
 def add_variables(prob, graph):
-    variable_dict = {}
     for idx, pair in enumerate(unique_pairs):
         # Generate all paths with a maximum length of 4
-        paths = nx.all_simple_paths(graph, source=pair[0], target=pair[1], cutoff=6)
+        paths = nx.all_simple_paths(graph, source=pair[0], target=pair[1], cutoff=5)
         for path in paths:
-            cost, pathname = compute_cost(graph, path)
-            if cost != np.inf:  # only add variables with finite cost
-                column = [cplex.SparsePair(ind=[num_nodes + 1 + idx], val=[1.0])]
-                var = prob.variables.add(obj=[cost], lb=[0.0], ub=[1.0], types=['C'],
-                                         columns=column, names=[pathname])
-                variable_dict[var] = path
+            if sum(isinstance(node, str) for node in path) == 2 and len(path) > 2:
+                # Path does not visit any cities intermediately and has length of at least 3
+                cost, pathname = compute_cost(graph, path)
+                # Add path without repeaters
+                column = [cplex.SparsePair(ind=['PairCon_' + ''.join(pair)], val=[1.0])]
+                prob.variables.add(obj=[cost], lb=[0.0], ub=[1.0], types=['B'],
+                                   columns=column, names=[pathname])
+                # Now iterate and generate paths with repeater
+                for node in path[1:-1]:
+                    rep_pathname = pathname + '___R' + str(node)
+                    column_contributions = [cplex.SparsePair(ind=['LinkCon_'+str(node), 'PairCon_' + ''.join(pair)],
+                                                             val=[1.0, 1.0])]
+                    prob.variables.add(obj=[cost-10], lb=[0.0], ub=[1.0], types=['B'],
+                                       columns=column_contributions, names=[rep_pathname])
 
 
 def compute_cost(graph, path):
@@ -107,16 +129,10 @@ def compute_cost(graph, path):
 
 
 if __name__ == "__main__":
-    graph = create_graph(draw=False)
-    print('Shortest Dijkstra paths:')
-    tot_cost = 0
-    for pair in unique_pairs:
-        shortest_path = nx.shortest_path(graph, source=pair[0], target=pair[1], weight='weight')
-        cost, _ = compute_cost(graph, shortest_path)
-        tot_cost += cost
-        print(shortest_path, 'with cost', cost)
-    print('Sum of shortest path costs:', tot_cost)
-    solve_cplex(graph=graph)
+    graph, color_map, mapping = create_graph(draw=False)
+    solve_cplex(graph=graph, color_map=color_map, mapping=mapping)
+
+
 
 
 
