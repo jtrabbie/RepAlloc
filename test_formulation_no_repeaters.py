2261ce2e0317e1f4c085995af13ba34bd8743992
julian.rabbie-tudelft <j.t.rabbie@student.tudelft.nl>
2019-08-28 13:23:23 +0200

    Fully working side by side comparison without repeaters

--- test_formulation_no_repeaters.py
+++ test_formulation_no_repeaters.py
@@ -3,8 +3,6 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
 import itertools
-import collections
-
 
 city_list = ['Lei', 'Haa', 'Ams', 'Del']
 unique_pairs = list(itertools.combinations(city_list, r=2))
@@ -16,15 +14,15 @@ def create_graph(draw=False):
     np.random.seed(1234)
     # Create a graph and add random weights
     graph = nx.fast_gnp_random_graph(n=20, p=0.4, seed=np.random)
+    # Change nodes to four Dutch cities
+    mapping = {3: city_list[0], 7: city_list[1], 4: city_list[2], 19: city_list[3]}
+    graph = nx.relabel_nodes(graph, mapping)
     for (u, v) in graph.edges():
         if u in city_list and v in city_list:
             # Set weight from city to city directly to inf
             graph.edges[u, v]['weight'] = np.inf
         else:
             graph.edges[u, v]['weight'] = np.random.randint(1, 10)
-    # Add nodes of four Dutch cities
-    mapping = {3: city_list[0], 7: city_list[1], 4: city_list[2], 19: city_list[3]}
-    graph = nx.relabel_nodes(graph, mapping)
     if draw:
         color_map = ['blue'] * len(graph.nodes)
         for idx, node in enumerate(graph.nodes):
@@ -51,8 +49,10 @@ def solve_cplex(graph):
     prob.solve()
     for i, x in enumerate(prob.solution.get_values()):
         if x == 1.0:
-            print('Variable in solution:', prob.variables.get_names(i))
+            path = prob.variables.get_names(i)
+            print('Chosen path:', path, 'with cost', prob.objective.get_linear(path))
 
+    print('Optimal objective value:', prob.solution.get_objective_value())
     return
 
 
@@ -63,40 +63,11 @@ def add_variables(prob, graph):
         paths = nx.all_simple_paths(graph, source=pair[0], target=pair[1], cutoff=6)
         for path in paths:
             cost, pathname = compute_cost(graph, path)
-            column = [cplex.SparsePair(ind=[idx], val=[1.0])]
-            var = prob.variables.add(obj=[cost], lb=[0.0], ub=[1.0], types=[prob.variables.type.binary],
-                                     columns=column, names=[pathname])
-            variable_dict[var] = path
-        #print('All paths added for', pair)
-
-
-def generate_paths(graph, source, sink):
-    a=1
-    return
-    # visited =1
-    #
-    #
-    # def printAllPathsUtil(self, u, d, visited, path):
-    #
-    #     # Mark the current node as visited and store in path
-    #     visited[u] = True
-    #     path.append(u)
-    #
-    #     # If current vertex is same as destination, then print
-    #     # current path[]
-    #     if u == d:
-    #         print
-    #         path
-    #     else:
-    #         # If current vertex is not destination
-    #         # Recur for all the vertices adjacent to this vertex
-    #         for i in self.graph[u]:
-    #             if visited[i] == False:
-    #                 self.printAllPathsUtil(i, d, visited, path)
-    #
-    #                 # Remove current vertex from path[] and mark it as unvisited
-    #     path.pop()
-    #     visited[u] = False
+            if cost != np.inf:  # only add variables with finite cost
+                column = [cplex.SparsePair(ind=[idx], val=[1.0])]
+                var = prob.variables.add(obj=[cost], lb=[0.0], ub=[1.0], types=[prob.variables.type.binary],
+                                         columns=column, names=[pathname])
+                variable_dict[var] = path
 
 
 def compute_cost(graph, path):
@@ -113,12 +84,14 @@ def compute_cost(graph, path):
 if __name__ == "__main__":
     graph = create_graph(draw=False)
     print('Shortest Dijkstra paths:')
+    tot_cost = 0
     for pair in unique_pairs:
-        print(nx.shortest_path(graph, source=pair[0], target=pair[1], weight='weight'))
-
+        shortest_path = nx.shortest_path(graph, source=pair[0], target=pair[1], weight='weight')
+        cost, _ = compute_cost(graph, shortest_path)
+        tot_cost += cost
+        print(shortest_path, 'with cost', cost)
+    print('Sum of shortest path costs:', tot_cost)
     solve_cplex(graph=graph)
-
-    print(graph['Ams'])
 
 
 
